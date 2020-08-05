3a5499e19f03dcd1cc2791c34b546f564791fbcc
Anjali Pal <39878805+Anjali001@users.noreply.github.com>
2020-08-05 23:37:14 +0530

    Add files via upload

---
+++ water.py
@@ -0,0 +1,13 @@
+import PyPDF2
+input_file = PyPDF2.PdfFileReader(open("one.pdf",'rb'))
+watermark_file = PyPDF2.PdfFileReader(open("wtr.pdf",'rb'))
+Output_file = PyPDF2.PdfFileWriter()
+for i in range(input_file.getNumPages()):
+    page = input_file.getPage(i)
+    wtr =watermark_file.getPage(0)
+    page.mergePage(wtr)
+    Output_file.addPage(page)
+
+with open("Yoyo.pdf",'wb') as output :
+    Output_file.write(output)
+
