435b8482e1df00e85d5520a738cb0e4a5913e134
Anjali Pal <39878805+Anjali001@users.noreply.github.com>
2020-08-07 17:19:20 +0530

    Update water.py

--- water.py
+++ water.py
@@ -1,13 +1,13 @@
 import PyPDF2
-input_file = PyPDF2.PdfFileReader(open("261852'rb'))
-watermark_file = PyPDF2.PdfFileReader(open("wtr.pdf",'rb'))
-Output_file = PyPDF2.PdfFileWriter()
-for i in range(input_file.getNumPages()):
-    page = input_file.getPage(i)
-    wtr =watermark_file.getPage(0)
-    page.mergePage(wtr)
-    Output_file.addPage(page)
+input_file = PyPDF2.PdfFileReader(open("261852'rb')) #Write the input_file here
+watermark_file = PyPDF2.PdfFileReader(open("wtr.pdf",'rb')) # This is the file that has wateramrk
+Output_file = PyPDF2.PdfFileWriter() 
+for i in range(input_file.getNumPages()): # Looping over all pages of input file
+    page = input_file.getPage(i) #getting a page of input file
+    wtr =watermark_file.getPage(0) # Getting watermark page
+    page.mergePage(wtr) # Putting watermark on input file page
+    Output_file.addPage(page) # added merged page to new pdf
 
 with open("Yoyo.pdf",'wb') as output :
-    Output_file.write(output)
+    Output_file.write(output) 
 
